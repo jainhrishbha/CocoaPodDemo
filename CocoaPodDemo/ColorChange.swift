82572e9a85684cb01072965b77c264aaadcd0958
hrishbhajain <hrishbhajain@magenative.com>
2021-03-17 10:13:07 +0530

    "changes again"

---
+++ CocoaPodDemo/ColorChange.swift
@@ -0,0 +1,25 @@
+//
+//  ColorChange.swift
+//  CocoaPodDemo
+//
+//  Created by cedcoss on 17/03/21.
+//
+
+import Foundation
+import UIKit
+public class ColorChange : UIViewController {
+    let colors : [UIColor] = [.red, .green, .orange , .black, .cyan, .darkGray, .blue , .gray]
+    var colorCounter = 1
+    public func colorChanged(){
+        let colorChangeTool = Timer.scheduledTimer(withTimeInterval: 5.0, repeats: true) { _ in
+            UIView.animate(withDuration: 5.0) {
+                self.view.backgroundColor = self.colors[self.colorCounter % 6]
+                self.colorCounter += 1
+                
+            }
+            
+        }
+        
+        colorChangeTool.fire()
+    }
+}
